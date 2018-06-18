96ac2211673f0520331d62ea14249249303b8784
Katie Maxwell <katie@codeinstitute.net>
2018-06-18 13:31:56 +0000

    messages working on login and register

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.base import TemplateView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import RegistrationForm
@@ -10,9 +11,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
-class LoginView(LoginView):
+class LoginView(SuccessMessageMixin, LoginView):
     template_name = 'accounts/login.html'
     form_class = AuthenticationForm
+    success_message = "You have been logged in"
 
 class LogoutView(LogoutView):
     next_page = "/"
@@ -20,10 +22,11 @@ class LogoutView(LogoutView):
 class ProfileView(TemplateView):
     template_name = "accounts/profile.html"
     
-class RegistrationView(CreateView):
+class RegistrationView(SuccessMessageMixin, CreateView):
     form_class = RegistrationForm
     success_url = reverse_lazy('profile')
     template_name = 'accounts/register.html'
+    success_message = "Thank you for registering!"
 
     def form_valid(self, form):
         valid = super(RegistrationView, self).form_valid(form)
