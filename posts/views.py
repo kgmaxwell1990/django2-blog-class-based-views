bec0475800a9b91a78c4ab6c8a8e21989a26622a
Katie Maxwell <katie@codeinstitute.net>
2018-06-19 13:56:20 +0000

    403 error page when non-authorized users try to edit/delete posts

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.http import HttpResponseForbidden
         
 class ShowPostList(ListView):
     model = Post
@@ -15,6 +16,7 @@ class ShowPostItem(DetailView):
 class AddPostItem(CreateView):
     model = Post
     fields = ['title', 'content', 'image']
+    
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
@@ -22,14 +24,19 @@ class AddPostItem(CreateView):
 class EditPostItem(UpdateView):
     model = Post
     fields = ['title', 'content', 'image']
-    def get_queryset(self):
-        print(self.request.user.is_superuser)
-        base_qs = super(EditPostItem, self).get_queryset()
-        return base_qs.filter(author=self.request.user)
-        
+    
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if not(self.request.user == obj.author or self.request.user.is_superuser):
+            return HttpResponseForbidden()
+        return super(EditPostItem, self).dispatch(request, *args, **kwargs)
+
 class DeletePostItem(DeleteView):
     model = Post
     success_url = reverse_lazy('posts')
-    def get_queryset(self):
-        base_qs = super(DeletePostItem, self).get_queryset()
-        return base_qs.filter(author=self.request.user)
+    
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if not(self.request.user == obj.author or self.request.user.is_superuser):
+            return HttpResponseForbidden()
+        return super(DeletePostItem, self).dispatch(request, *args, **kwargs)
